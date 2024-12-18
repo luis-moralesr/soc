520eb6a78ef8023b3175b65ac68ad7cd12027b89
Luis Morales <luis.moralesr90@gmail.com>
2024-12-17 23:08:11 -0600

    Se inicializa sistema Laravel para SOC Asesores

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        if (!User::where('email', 'luis@gmail.com')->exists()) {
+            User::create([
+                'name' => 'Luis Ángel',
+                'lastName' => 'Morales Romero',
+                'email' => 'luis@gmail.com',
+                'company' => 'SOC Asesores',
+                'state' => 'Mexico',
+                'password' => Hash::make('asesores'),
+                'rol' => 'admin',
+            ]);
+        }
+    }
+}
